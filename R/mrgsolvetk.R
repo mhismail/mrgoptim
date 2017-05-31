856d3fdc7119d76afa862380378cd396fd36bf29
Kyle Baron <kyleb@metrumrg.com>
2017-05-31 07:50:33 -0500

    re-configure runif/rnorm sens

--- R/mrgsolvetk.R
+++ R/mrgsolvetk.R
@@ -1,31 +1,22 @@
-
+##' Generate idata set for sensitivity analysis.
+##' 
+##' @param 
 ##' @export
-sens_unif_idata <- function(mod,pars,lower=0.2,upper=3,n=100,
+sens_unif_idata <- function(pars,lower=0.2,upper=3,n=100,
                             spread=TRUE,...) {
-  pars <- mrgsolve:::cvec_cs(pars)
-  params <- as.numeric(param(mod))[pars]
-  parmin <- params*lower
-  parmax <- params*upper
-  out <- mvuniform(n,pars,params*lower,params*upper)
+  out <- mvuniform(n,pars,pars*lower,pars*upper)
   out <- cbind(data_frame(.n=1:n),out)
   if(!spread) out <- gather(out,par,value,2:ncol(out)) %>% mutate(.n=1:n())
   out
 }
 
 ##' @export
-sens_norm_idata <- function(mod,pars,cv,n=100,
+sens_norm_idata <- function(pars,cv,
+                            n=100,
                             log = TRUE,
                             spread=TRUE,...) {
-  pars <- mrgsolve:::cvec_cs(pars)
-  params <- as.numeric(param(mod))[pars]
-  cv <- as.matrix(cv/100)
-  if(nrow(cv) != length(params)) {
-    if(length(cv)==1) cv <- diag(rep(cv,length(params)))
-  } else {
-    stop("Wrong length for cv") 
-  }
-  
-  out <- MASS::mvrnorm(n,log(params),cv)
+  cv <- diag(rep((cv/100)^2,length(pars)))
+  out <- MASS::mvrnorm(n,log(pars),cv)
   out <- exp(out)
   out <- cbind(matrix(1:nrow(out),ncol=1, dimnames=list(NULL,".n")),out)
   out <- as.data.frame(out)
@@ -35,8 +26,10 @@ sens_norm_idata <- function(mod,pars,cv,n=100,
 
 
 ##' @export
-sens_unif <- function(mod,n=100,...) {
-  data <- sens_unif_idata(mod=mod,n=n,...)
+sens_unif <- function(mod,n=100,pars=names(param(mod)),...) {
+  pars <- mrgsolve:::cvec_cs(pars)
+  pars <- as.numeric(param(mod))[pars]
+  data <- sens_unif_idata(pars=pars,n=n,...)
   out <- mrgsim(mod,idata=data,carry.out=".n",obsonly=TRUE,...)
   out <- left_join(as.tbl(out),data,by=".n")
   out
@@ -46,19 +39,19 @@ sens_unif <- function(mod,n=100,...) {
 ##' 
 ##' @param mod the model object
 ##' @export
-sens_norm <- function(mod,n=100,...) {
-  data <- sens_norm_idata(mod=mod,n=n,...)
+sens_norm <- function(mod,n=100,pars=names(param(mod)),...) {
+  pars <- mrgsolve:::cvec_cs(pars)
+  pars <- as.numeric(param(mod))[pars]
+  data <- sens_norm_idata(mod=mod,n=n,pars=pars,...)
   out <- mrgsim(mod,idata=data,carry.out=".n",obsonly=TRUE,...)
   out <- left_join(as.tbl(out),data,by=".n")
   out
 }
 
-
-
-
 mvuniform <- function(n,par,a,b,...) {
+  parn <- names(par)
   out <- lapply(seq_along(par), function(i) {
-    setNames(data_frame(runif(n,a[i],b[i])),par[i])
+    setNames(data_frame(runif(n,a[i],b[i])),parn[i])
   })
   out <- bind_cols(out)
   out
