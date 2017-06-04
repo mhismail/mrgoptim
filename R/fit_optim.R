d51609a270eac6e5d51537685100e7a6a2a380ed
Kyle Baron <kyleb@metrumrg.com>
2017-06-04 07:48:03 -0500

    convert fit_optim to parset)

--- R/fit_optim.R
+++ R/fit_optim.R
@@ -1,17 +1,21 @@
+##' @importFrom optimhelp graft
+##' @importClassesFrom optimhelp parset 
+##' @importMethodsFrom optimhelp initials as.list
+
 is_error <- function(x) {
   inherits(x,"try-error") 
 }
 
-predict_optim <- function(pars,mod,data,dv = "DV", pred = "PRED",.ofv,
+predict_optim <- function(pars,mod,data,dv = "DV", pred = "PRED",.ofv,p,
                           dvindex,get_pred=FALSE,...) {
-  pars <- lapply(pars,exp)
-  mod <- param(mod,pars)
-  out <- dplyr::as_data_frame(mrgsim(mod,data=data,carry.out="evid",param=pars))
+  p  <- optimhelp::graft(p,pars)
+  p <- as.list(p)
+  out <- dplyr::as_data_frame(mrgsim(mod,data=data,carry.out="evid",param=p))
   if(is_error(out)) {
     stop("failure in predict function",call.=FALSE) 
   }
   if(get_pred) return(as.data.frame(dplyr::as_data_frame(out)))
- .ofv(data[dvindex,dv],out[dvindex,pred],pars)
+ .ofv(data[dvindex,dv],out[dvindex,pred],p)
 }
 
 ##' Fit a model to data using optim.
@@ -24,18 +28,19 @@ fit_optim <- function(mod,data=NULL,dv="DV",pred="DV",.ofv,par,...) {
     data[,"..zeros.."] <- NULL
     mod@args$data <- NULL 
   }
-  fit <- optim(par=par,
+  fit <- optim(par=initials(par),
                fn=predict_optim,
                mod=mod,
                data=data,
                dv=dv,
                pred=pred,
                .ofv=.ofv,
+               p=par,
                dvindex = which(data$evid==0),
                ...)
-  out <- predict_optim(fit$par,mod=mod,data=data,get_pred=TRUE)
+  out <- predict_optim(fit$par,mod=mod,data=data,p=par,get_pred=TRUE)
   data[,"PRED"] <- out[,pred]
-  return(list(fit=fit,tab=data,out=out))
+  return(c(fit,list(tab=data)))
 }
 
 
