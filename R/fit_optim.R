9d4f236ca3f5173c9b52ec648698048c08a8acbb
Kyle Baron <kyleb@metrumrg.com>
2017-06-04 20:10:53 -0500

    require specific arguments for ofv

--- R/fit_optim.R
+++ R/fit_optim.R
@@ -1,4 +1,4 @@
-##' @importFrom optimhelp graft
+##' @importFrom optimhelp graft require_par
 ##' @importClassesFrom optimhelp parset 
 ##' @importMethodsFrom optimhelp initials as.list
 
@@ -6,7 +6,14 @@ is_error <- function(x) {
   inherits(x,"try-error") 
 }
 
-predict_optim <- function(pars,mod,data,dv = "DV", pred = "PRED",.ofv,p,
+check_ofv_formals <- function(ofv) {
+  if(!identical(names(formals(ofv)),c("dv", "pred", "par"))) {
+    stop("the formal arguments for ofv must be dv, pred, and par",call.=FALSE) 
+  }
+}
+
+
+predict_optim <- function(pars,mod,data,dv = "DV", pred = "PRED",ofv,p,
                           dvindex,get_pred=FALSE,...) {
   p  <- optimhelp::graft(p,pars)
   p <- as.list(p)
@@ -15,26 +22,27 @@ predict_optim <- function(pars,mod,data,dv = "DV", pred = "PRED",.ofv,p,
     stop("failure in predict function",call.=FALSE) 
   }
   if(get_pred) return(as.data.frame(dplyr::as_data_frame(out)))
- .ofv(data[dvindex,dv],unlist(out[dvindex,pred],use.names=FALSE),p)
+  ofv(data[dvindex,dv],unlist(out[dvindex,pred],use.names=FALSE),p)
 }
 
 ##' Fit a model to data using optim.
 ##' 
 ##' @export
 ##' @rdname fit_optim
-fit_optim <- function(mod,data=NULL,dv="DV",pred="DV",.ofv,par,...) {
+fit_optim <- function(mod,data=NULL,dv="DV",pred="DV",ofv,par,...) {
   if(is.null(data)) {
     data <- as.data.frame(unclass(mod@args$data))
     data[,"..zeros.."] <- NULL
     mod@args$data <- NULL 
   }
+  check_ofv_formals(ofv)
   fit <- optim(par=initials(par),
                fn=predict_optim,
                mod=mod,
                data=data,
                dv=dv,
                pred=pred,
-               .ofv=.ofv,
+               ofv=ofv,
                p=par,
                dvindex = which(data$evid==0),
                ...)
