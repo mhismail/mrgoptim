ac24860dfdfb2f2630387ba3bb44d8242936455f
Mohamed Ismail <mhismail@buffalo.edu>
2018-04-21 19:08:05 -0400

    Refactor mrgoptim

--- R/mrgoptim.R
+++ R/mrgoptim.R
@@ -1,8 +1,9 @@
-
+# Negative log likelihood
 ML_obj_fun <- function(raw,pred,var) {
   1 / 2 * sum(((raw - pred) * (raw - pred)) / var +
             log(var)) + 1 / 2 * length(raw) * log(2 * pi)
 }
+
 
 get_preds <- function (mod, params, Request) {
   out <- mod %>%
@@ -34,6 +35,7 @@ ML_optim <- function(params, mod, output, var, input) {
 
 # Covariance step functions -----------------------------------------------
 
+# Step size for finite difference approximations
 h <- function (atol, rtol, param) {
     (rtol) ^ (1 / 3) * max(param, atol)
 }
@@ -41,21 +43,21 @@ h <- function (atol, rtol, param) {
 hj <- Vectorize(h, c("param"))
 
 get_matrices <- function(params, mod, output, var, input){
-  a <- mod@args$data
-  class(a) <- NULL
+  mod_list <- as.list(mod)
+  a <- unclass(simargs(mod)$data)
   a <- as.data.frame(a)
-  a <- a[a$evid == 0,2]
+  a <- a[a$evid == 0, 2]
   
   dydtheta <- matrix(0, nrow = length(a), ncol = length(params))
   dgdtheta <- matrix(0, nrow = length(a), ncol = length(params))
 
-  hja <- hj(mod@atol, mod@rtol, params)
+  hja <- hj(unname(mod_list$solver["atol"]), unname(mod_list$solver["rtol"]), params)
   Request <- c(output, var)
   
   for (i in 1:length(params)) {
     new_params1 <- params
     new_params2 <- params
-
+    
 
     new_params1[i] <- params[i] + hja[i]
     new_params2[i] <- params[i] - hja[i]
@@ -113,62 +115,69 @@ mrgoptim <- function(mod,
                      method = "newuoa",
                      options = list(maxit = 10000)) {
   
-  data <- as.data.frame(unclass(mod@args$data))
+  mod_list <- as.list(mod)
   
-  if(!is.mrgmod(mod)) {
+  data <- as.data.frame(unclass(simargs(mod)$data))
+  
+  if (!is.mrgmod(mod)) {
     stop("mod argument must be a model object", call. = FALSE) 
   }
   
-  if(is.null(dim(data))) {
+  if (is.null(dim(data))) {
     stop("dataset not found", call. = FALSE) 
   }
   
-  if(!all(output %in% c(names(mod@init@data), mod@capture))) {
+  if (!all(output %in% c(mod_list$cmt, mod_list$capture))) {
     stop("couldn't find ",
-         paste(output[which(!output %in% c(names(mod@init@data), mod@capture))], collapse=", "), 
+         paste(output[which(!output %in% c(mod_list$cmt, mod_list$capture))], collapse=", "), 
          " in model output", call. = FALSE) 
   }
   
-  if(!all(var %in% c(names(mod@init@data), mod@capture))) {
+  if (!all(var %in% c(mod_list$cmt, mod_list$capture))) {
     stop("couldn't find ",
-         paste(output[which(!var %in% c(names(mod@init@data), mod@capture))], collapse=", "), 
+         paste(output[which(!var %in% c(mod_list$cmt, mod_list$capture))], collapse=", "), 
          " in model output", call. = FALSE) 
   }
   
-  if(!all(input %in% data$cmt)) {
+  if (!all(input %in% data$cmt)) {
     stop("couldn't find compartment/s ",
          paste(input[which(!input %in% data$cmt)], collapse=", "), 
          " in cmt column", call. = FALSE) 
   }
   
-  if(!(length(input) == length(output) & length(input) == length(var))){
+  if (!(length(input) == length(output) & length(input) == length(var))){
     stop("input, output, and var vectors should have same length", call. = FALSE)
   }
   
-  if(!all(prms %in% names(mod@param))){
+  if (!all(prms %in% names(mod_list$param))){
     stop("couldn't find ",
-         paste(prms[which(!prms %in% names(mod@param))], collapse=", "),
+         paste(prms[which(!prms %in% names(mod_list$param))], collapse=", "),
          " in parameter list", call. = FALSE) 
   }
   
-  if(!all(v_prms %in% names(mod@param))){
+  if (!all(v_prms %in% names(mod_list$param))){
     stop("couldn't find ",
-         paste(v_prms[which(!v_prms %in% names(mod@param))], collapse=", "),
+         paste(v_prms[which(!v_prms %in% names(mod_list$param))], collapse=", "),
          " in parameter list",call. = FALSE) 
   }
   
   #Get all parameters in mod object
-  params <- as.list(mod)$param
+  params <- mod_list$param
   
-  #Select only parameters to be estimated
+  # Select only parameters to be estimated
   params <- params[which(names(params) %in% c(prms,v_prms))] %>%
     unlist() %>% log10()
   
   
   fit <-list()
+  
+  # Option to restart optimization with final estimates
   for (i in 1:(1 + restarts)) {
+    
+    # Initial optmization
     if (i == 1) {
-      fit[[1]] <- if(method != "newuoa"){
+      fit[[1]] <- if (method != "newuoa") {
+        
         # Fit with optim
         optim(par = params,
               fn = ML_optim,
@@ -179,6 +188,7 @@ mrgoptim <- function(mod,
               var,
               input,
               control = options)} else {
+                
         # Fit with newuoa
         minqa::newuoa(
               par = params,
@@ -188,9 +198,14 @@ mrgoptim <- function(mod,
               var = var,
               input = input)
               }
+      
+       # Optimization with newuoa strips names in output, add them back in
        names(fit[[1]]$par) <- names(params)
+       
+    # Optimization with final estimates of previous run   
     } else {
-      fit[[i]] <- if(method != "newuoa") {
+    
+      fit[[i]] <- if (method != "newuoa") {
         optim(par = fit[[i - 1]]$par,
               fn = ML_optim,
               gr = NULL,
@@ -200,6 +215,7 @@ mrgoptim <- function(mod,
               var,
               input,
               control = options)} else {
+                
         minqa::newuoa(
           par = fit[[i - 1]]$par,
           fn = ML_optim, 
@@ -207,29 +223,44 @@ mrgoptim <- function(mod,
           output = output,
           var = var,
           input = input)
-      }
+              }
+      
       names(fit[[i]]$par) <- names(params)
     }
   }
   
-  # Cov step
+  # Covariance step
   if (cov) { 
     
     params <- 10 ^ fit[[1 + restarts]]$par
+    
+    
     mats<- get_matrices(params, mod, output, var, input)
     
     M <- matrix (0, nrow = length(params), ncol = length(params))
+    
+    # n x p matrix
     dydtheta.all <- mats[[1]]
+    
+    # n x p matrix
     dgdtheta.all <- mats[[2]]
+    
+    # n-length vector
     variances <- reformat(get_preds(mod, params, c(output,var)), input, output)[, 5 + length(output)]
     
+    # which parameters are system parameters
     thetas <- which(names(params) %in% c(prms))
+    
+    # M is Fisher information matrix
     M <- 1 / 2 * (t(dgdtheta.all/(c(variances ^ 2))) %*% (dgdtheta.all))
-    M[thetas, thetas] <- (M[thetas, thetas] + t(dydtheta.all[, thetas] / c(variances)) %*% (dydtheta.all[, thetas]))
+    M[thetas, thetas] <- (M[thetas, thetas] + 
+                            t(dydtheta.all[, thetas] / c(variances)) %*% (dydtheta.all[, thetas]))
+    
     
     cov <- tryCatch(solve(M), error = function(err) "Error")
     
-    if(is.matrix(cov)) {
+    
+    if (is.matrix(cov)) {
       fit[[1 + restarts]]$cov <- cov
       fit[[1 + restarts]]$cor <- cov2cor(cov)
       fit[[1 + restarts]]$CVPercent <- sqrt(diag(cov)) / params * 100
